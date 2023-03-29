5759d702f8d938009a0c67ef21b86df4444226e7
Bran <jiakuo.zhang@outlook.com>
2023-03-29 18:27:05 +0800

    ud

---
+++ App/Jobs/SignRemainder.cs
@@ -0,0 +1,9 @@
+namespace Jobs;
+
+public class SignRemainder : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Test/LibTest.cs
@@ -0,0 +1,10 @@
+namespace Test;
+
+[TestClass]
+public class LibTest
+{
+    [TestMethod]
+    public void TestSendEmail()
+    {
+    }
+}
