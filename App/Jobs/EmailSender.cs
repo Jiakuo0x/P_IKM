b62e8b0330e98d9d33e9d1735da0793328116ac4
bran <jiakuo.zhang@quest-global.com>
2023-04-26 11:39:00 +0800

    bug fix

--- App/Jobs/EmailSender.cs
+++ App/Jobs/EmailSender.cs
@@ -24,8 +24,6 @@ public class EmailSender : BackgroundService
     {
         while (true)
         {
-            _logger.LogInformation($"[{DateTime.Now}] Start Job: {nameof(EmailSender)}");
-
             using (var scope = _scopeFactory.CreateScope())
             {
                 _templateMappingService = scope.ServiceProvider.GetRequiredService<TemplateMappingService>();
@@ -45,8 +43,6 @@ public class EmailSender : BackgroundService
                     await Task.Delay(TimeSpan.FromSeconds(10));
                 }
             }
-
-            _logger.LogInformation($"[{DateTime.Now}] End Job: {nameof(EmailSender)}");
         }
     }
     protected async Task DoWork()
