0c73970ea1d2bde6b256b4c7e85f357fdf330c35
bran <jiakuo.zhang@quest-global.com>
2023-04-26 12:05:51 +0800

    Added some comments

--- Database/Enums/TaskStep.cs
+++ Database/Enums/TaskStep.cs
@@ -11,16 +11,39 @@ public enum TaskStep
     Unknown,
 
     /// <summary>
-    /// Contract creating when the envelope has been analyse, and waiting for create a contract to Bestsign
+    /// When the envelope has been parsed and is waiting for the creation of a Bsetsign Contract, the status is ContractCreating.
     /// </summary>
     ContractCreating,
+
+    /// <summary>
+    /// When the contract has been created in Bestsign and is waiting for internal processing within the Bestsign system, the status is ContractCreated.
+    /// </summary>
     ContractCreated,
 
+    /// <summary>
+    /// When an envelope is voided within DocuSign System, the status becomes ContractCancelling.
+    /// And the system waits for cancellation of the corresponding contract within the Bestsign system.
+    /// </summary>
     ContractCancelling,
+
+    /// <summary>
+    /// When the status is ContractCancelling and the corresponding contract in Bestsign system has been cancelled, the status changes to ContractCancelled.
+    /// </summary>
     ContractCancelled,
 
+    /// <summary>
+    /// When the contract is signed and completed in Bestsign system, the status is ContractComplete.
+    /// </summary>
     ContractCompleted,
+
+    /// <summary>
+    /// When a step in the system encounters five consecutive errors during runtime, the status becomes Failed.
+    /// And the system waits to send email notifications to relevant personnel.
+    /// </summary>
     Failed,
 
+    /// <summary>
+    /// When the task has ended, the status is Completed. This status indicates the final state.
+    /// </summary>
     Completed,
 }
